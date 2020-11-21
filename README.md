2c5600085a71d49a40c8205f395c7958ae05cbe9
Sathishchary <ksathish@xtremand.com>
2020-11-21 15:00:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-gwxxgv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-gwxxgv)
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  name = "Angular";
+  array = [1, "2", 6, 0.5, "6366"];
+}

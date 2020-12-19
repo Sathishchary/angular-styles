b38852fba693929463dd7acf9bfa555a8f6bccef
Sathishchary <ksathish@xtremand.com>
2020-12-19 22:44:49 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  name = "Angular";
+export class AppComponent implements OnInit{
+  name = null;
   array = [1, "2", 6, 0.5, "6366"];
+
+  ngOnInit(){
+  this.name ??= 'Angular';
+  }
 }
